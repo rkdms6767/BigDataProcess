9b7bb9f2c15a50b98a0b4effdeda4c588c9736e0
rkdms6767 <rkdms5991@naver.com>
2023-11-03 23:50:15 +0900

    코드 수정

--- HW1/student20200975.py
+++ HW1/student20200975.py
@@ -7,28 +7,26 @@ def grade(total, totallist, num):
     #학점을 받을 수 있는 학생 수
     A = int(0.3 * num)
     B = int(0.7 * num)
-    A_Plus = int(0.5 * A)
-    B_Plus = int(0.5 * B)
-    C_Plus = int(0.5 * (0.9 * num))
+    A_Plus = min(A, int(0.5 * A))
+    B_Plus = min(B, int(0.5 * B))
+    C_Plus = min(int(0.5 * (0.9 * num)))
     
 
     #(totallist)학점을 받을 수 있는 마지막 학생의 점수와 각 행의 total의 점수를 비교해서 학점 넣기
     if total < 40:
         return 'F'
-    elif total >= totallist[A_Plus - 1]:
-        return 'A+'
-    elif total >= totallist[A - 1]:
-        return 'A'
+    elif total >= totallist[int(0.9 * num) -1]:
+        return 'C'
+    elif total >= totallist[C_Plus - 1]:
+        return 'C+'
+    elif  total >= totallist[B - 1]:
+        return 'B'
     elif total >= totallist[B_Plus - 1]:
         return 'B+'
-    elif total >= totallist[B - 1]:
-        return 'B'
-    elif total >= totallist[C_Plus - 1]:
-        return 'A+'
-    elif total >= totallist[C_Plus -1]:
-        return 'C+'
+    elif total >= totallist[A - 1]:
+        return 'A'
     else:
-        return 'C'
+        return 'A+'
      
 wb = load_workbook(r'C:\python\02_python엑셀\student.xlsx')
 ws = wb.active
@@ -37,7 +35,7 @@ totallist = []
 
 # 두번째 행부터 total구하기
 for row in ws.iter_rows(min_row=2):
-    total = ((row[2].value*0.3) + (row[3].value*0.35) + (row[4].value*0.34) + (row[5].value*0.1))
+    total = ((row[2].value*0.3) + (row[3].value*0.35) + (row[4].value*0.34) + (row[5].value*0.1))+0.9
     row[6].value = total
 
 #total기준으로 정렬한 리스트 생성
